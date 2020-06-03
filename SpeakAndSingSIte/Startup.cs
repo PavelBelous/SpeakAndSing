340b366b1713768067570afa0f2dc200c7a05910
W_hit_e <pbelous@progredev.com>
2020-06-04 00:01:30 +0700

    отключил неспользуемые using

--- SpeakAndSingSIte/Startup.cs
+++ SpeakAndSingSIte/Startup.cs
@@ -1,13 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -35,7 +29,6 @@ namespace SpeacAndSingSIte
             services.AddTransient<ITeacherRepositories, EFTeachersRepositories>();
             services.AddTransient<DataManager>();
 
-            //подключаем базу данных
             services.AddDbContext<AppDbContext>(x => x.UseSqlServer(Config.ConnectionString));
 
             //настраиваем identity систему
